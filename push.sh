bf1114251c6d21b123b09ee40b7d6bf989895d05
OwenPendrighElliott <owenpendrighelliott@gmail.com>
2025-08-08 08:32:52 +1000

    remove arm64 for the moment

--- push.sh
+++ push.sh
@@ -8,12 +8,12 @@ IMAGE_NAME="owenpelliott/hnswlib_server"
 TAG="latest"
 FULL_IMAGE="$IMAGE_NAME:$TAG"
 
-# docker build -t $IMAGE_NAME:amd64 --platform linux/amd64 .
-# docker build -t $IMAGE_NAME:arm64 --platform linux/arm64 .
-# docker manifest create $IMAGE_NAME:latest \
-#     --amend $IMAGE_NAME:amd64 \
-#     --amend $IMAGE_NAME:arm64
-# docker manifest push $IMAGE_NAME:latest
+docker build -t $IMAGE_NAME:amd64 --platform linux/amd64 .
+docker build -t $IMAGE_NAME:arm64 --platform linux/arm64 .
+docker manifest create $IMAGE_NAME:latest \
+    --amend $IMAGE_NAME:amd64 \
+    --amend $IMAGE_NAME:arm64
+docker manifest push $IMAGE_NAME:latest
 
 docker build -t $IMAGE_NAME:latest --platform linux/arm64 .
 docker push $IMAGE_NAME:latest
